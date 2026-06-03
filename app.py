a8efc7eac3839e3a5dbb72ecf1b9d93ea382d444
chandraaulia336-coder <chandraaulia336@gmail.com>
2026-06-03 23:21:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def save_materi_sheets(data_list):
 
 # --- FUNGSI DATABASE GALERI ---
 def fetch_galeri_sheets():
-    if API_URL_GALERI == "PASTE_URL_APPS_SCRIPT_GALERI_LU": 
+    if API_URL_GALERI == "https://script.google.com/macros/s/AKfycbwIJXXeB58YCeWBqOwLZ5wtLv9Se901K5FaZS5-6YBIjt-I8dtDp1bCQoHgpd_AcF4z/exec": 
         st.warning("⚠️ URL Galeri belum lu ganti di app.py, bro!")
         return []
     try:
@@ -50,7 +50,7 @@ def fetch_galeri_sheets():
         return []
 
 def save_galeri_sheets(data_list):
-    if API_URL_GALERI != "PASTE_URL_APPS_SCRIPT_GALERI_LU":
+    if API_URL_GALERI != "https://script.google.com/macros/s/AKfycbwIJXXeB58YCeWBqOwLZ5wtLv9Se901K5FaZS5-6YBIjt-I8dtDp1bCQoHgpd_AcF4z/exec":
         try: 
             res = requests.post(API_URL_GALERI, json={"data": data_list})
             if res.status_code != 200:
